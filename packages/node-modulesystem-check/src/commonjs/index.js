31fe4787b6fa7d27c30a84a1c93fc458509e7ec1
Niclas Roßberger <niclas@nidomiro.de>
2023-06-20 07:15:50 +0200

    docs: adds jsdocs

--- packages/node-modulesystem-check/src/commonjs/index.js
+++ packages/node-modulesystem-check/src/commonjs/index.js
@@ -1,3 +1,6 @@
 
-
+/**
+ * Checks which module system is used to execute the current script.
+ * @returns { 'commonjs' | 'esm' }
+ */
 module.exports.getModuleSystem = () => 'commonjs';
--- packages/node-modulesystem-check/src/esm/index.js
+++ packages/node-modulesystem-check/src/esm/index.js
@@ -1,3 +1,6 @@
 
-
+/**
+ * Checks which module system is used to execute the current script.
+ * @returns { 'commonjs' | 'esm' }
+ */
 export const getModuleSystem = () => 'esm'
